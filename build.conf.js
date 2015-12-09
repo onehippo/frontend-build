15d0bbc80f35e12d1ef244ebd6b744997bfad650
Joeri de Gooijer <joerideg@gmail.com>
2015-12-09 14:57:33 +0100

    Add licenses

--- build.conf.js
+++ build.conf.js
@@ -1,8 +1,19 @@
 /*
- * This file/module contains all configuration for the build process.
+ * Copyright 2015 Hippo B.V. (http://www.onehippo.com)
  *
- * This is a collection of patterns. These paths are used in the configuration of build tasks.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 var objectAssign = require('lodash.assign');
 var appRootDir = require('app-root-dir');
 
