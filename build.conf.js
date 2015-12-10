582d3e9337a1a7ccec04435e56ecbdb1af6c672e
Joeri de Gooijer <joerideg@gmail.com>
2015-12-10 10:43:36 +0100

    update systemjs paths for karma-systemjs and update dependencies

--- build.conf.js
+++ build.conf.js
@@ -81,7 +81,14 @@ function buildConfig(customConfig) {
   };
   cfg.systemjsOptions = {
     transpiler: 'babel',
-    defaultJSExtensions: true
+    defaultJSExtensions: true,
+    paths: {
+      'babel': 'node_modules/babel-core/browser.js',
+      'systemjs': 'node_modules/systemjs/dist/system.js',
+      'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js',
+      'es6-module-loader': 'node_modules/es6-module-loader/dist/es6-module-loader.js',
+      'phantomjs-polyfill': 'node_modules/phantomjs-polyfill/bind-polyfill.js'
+    }
   };
 
   return objectAssign(cfg, customCfg);
